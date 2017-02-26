a88483a86d07672d6f04eb16972e2319bfe5d5ef
Gar <gar@lonelydatu.com>
2017-02-25 22:30:17 -0500

    one

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import Handwrite from 'handwrite'
+
+console.log(Handwrite);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
